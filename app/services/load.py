51d5648431921c73023cd4ab54acaed5e74a044a
aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-08-30 14:00:46 +0530

    load the merged dataset into the db

--- app/services/load.py
+++ app/services/load.py
@@ -1,21 +1,27 @@
-# app/services/load.py
 from sqlalchemy.orm import Session
-from app.db.models import ProcessedData
 from app.db.database import SessionLocal
+from app.db.models import MergedData
 from app.core.logging import logger
+import pandas as pd
 
-def load_data(datasets: dict):
+def load_data_to_db(merged_df: pd.DataFrame):
+    logger.info("Loading data into the database")
+    
     db: Session = SessionLocal()
     try:
-        for dataset_name, df in datasets.items():
-            for _, row in df.iterrows():
-                db_data = ProcessedData(dataset=dataset_name, **row.to_dict())
-                db.add(db_data)
+        for _, row in merged_df.iterrows():
+            db_record = MergedData(**row.to_dict())
+            db.add(db_record)
+        
         db.commit()
-        logger.info("Data successfully loaded to the database")
+        logger.info("Data successfully loaded into the database")
+    
     except Exception as e:
-        logger.error(f"Failed to load data: {str(e)}")
         db.rollback()
-        raise
+        logger.error(f"Error loading data into the database: {str(e)}")
+        raise e
+    
     finally:
         db.close()
+        logger.info("Database session closed")
+  
