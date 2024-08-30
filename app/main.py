d3533214d12c6e79a4109838b25099e79f896e5d
aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-08-30 12:46:01 +0530

    refactor: main file and code improvement

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
-from app.api import endpoints
+from contextlib import asynccontextmanager
+from app.db.database import create_tables, SessionLocal
+from app.api.endpoints import router as api_router
+from app.core.logging import logger
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Creating tables in the database if they do not exist...")
+    create_tables()
+    logger.info("Tables created successfully.")
+    
+    yield
+    
+    logger.info("Shutting down...")
 
+app = FastAPI(lifespan=lifespan)
 
-app.include_router(endpoints.router,  prefix="/api/v1")
+app.include_router(api_router, prefix="/api/v1")
 
 if __name__ == "__main__":
+    logger.info("Starting FastAPI server...")
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
