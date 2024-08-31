c7bc0209dd6a6f1c0a68efe9d8e91d0e675d7a5f
aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-08-31 14:12:18 +0530

    everything is working as per the instruction, needs more optimisation

--- app/services/custom_exceptions.py
+++
@@ -1,9 +0,0 @@
-class FileValidationError(Exception):
-    def __init__(self, message="Invalid file format. Only CSV and XLSX files are allowed."):
-        self.message = message
-        super().__init__(self.message)
-
-class DataProcessingError(Exception):
-    def __init__(self, message="Error processing data."):
-        self.message = message
-        super().__init__(self.message)
